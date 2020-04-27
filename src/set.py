01f7c88994a6e466fb2b3b5ddb03335db2b0e815
kangwenjun <603164555@qq.com>
2020-04-27 08:24:04 +0800

    set:成员关系

---
+++ src/set.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# coding=utf-8
+
+student = {'Tom', 'Jim', 'Mary', "Tom", "Jack", "Rose"}
+print(student) #集合自动去重
+
+#成员测试
+if "Rose" in student :
+	print("Rose 在集合中")
+else :
+	print("Rose 不在集合中")
+	
+a = set("abcfefdsg")
+b = set("sdfaerkej")
+print("a:\t", a)
+print("b:\t", b)
+print("a-b:\t", a-b) #在集合a当中，集合b没有的元素
+print("b-a:\t", b-a)
+print("a|b:\t", a|b)
+print("a&b:\t", a&b)
+print("a^b:\t", a^b)
