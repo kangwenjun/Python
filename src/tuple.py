387fe95e938d822acee3a97ae09eb368a772d546
kangwenjun <603164555@qq.com>
2020-05-03 10:41:51 +0800

    tuple

--- src/tuple.py
+++ src/tuple.py
@@ -1,24 +1,44 @@
 #! /usr/bin/env python
 # coding=utf-8
 
-tuple = (1, 1.2, "name", 1+2j)
-print(tuple)
-print(tuple[0:])
-print(tuple[-1::-1])
+print('\n----------------special tuple----------------')
+print("(1):\t", type((1)))
+print("(1,):\t", type((1,)))
+print("():\t", type(()))
 
-print(tuple[0])		#元素
-print(tuple[0:1])	#元组，tuple
+print('\n----------------all----------------')
+print('("kang", "wen", "jun")', ("kang", "wen", "jun"))
+print('("kang", "wen", "jun")[:]:\t', ("kang", "wen", "jun")[:])
+print('("kang", "wen", "jun")[0:]:\t', ("kang", "wen", "jun")[0:])
+print('("kang", "wen", "jun")[-1::-1]:\t', ("kang", "wen", "jun")[-1::-1])
 
-print(tuple[0:2])
-print(tuple[0::2])
+print('\n----------------some----------------')
+print('("kang", "wen", "jun")[0]:\t', ("kang", "wen", "jun")[0])
+print('("kang", "wen", "jun")[0:-1]:\t', ("kang", "wen", "jun")[0:-1])
+print('("kang", "wen", "jun")[0::2]:\t', ("kang", "wen", "jun")[0::2])
 
-print(tuple*2)
-print(tuple+tuple)
+print('\n----------------joint----------------')
+print('("kang", "wen", "jun")+("kang", "wen", "jun"):\t', ("kang", "wen", "jun")+("kang", "wen", "jun"))
+print('("kang", "wen", "jun")*2:\t', ("kang", "wen", "jun")*2)
 
-#tuple[0:2] = () #tuple不可以修改
+print('\n----------------iterater----------------')
+print('"kang" in ("kang", "wen", "jun"):\t', "kang" in ("kang", "wen", "jun"))
+for x in ("kang", "wen", "jun") :
+	print('tuple-for:\t', x)
+	
+print('\n----------------operator----------------')
+print('len(("kang", "wen", "jun")):\t', len(("kang", "wen", "jun")))
+print('max((3, 1, 7, 5, 9)):\t', max((3, 1, 7, 5, 9)))
+print('min((3, 1, 7, 5, 9)):\t', min((3, 1, 7, 5, 9)))
+print('type(tuple([1,2,3])):\t', type(tuple([1,2,3])))
+print('type(tuple((1,2,3))):\t', type(tuple((1,2,3))))
+print('type(tuple({1, 2, 3})):\t', type(tuple({1,2,3})))
+print('type(tuple({1:"Chinese", 2:"American", 3:"Italian"})):\t', type(tuple({1:"Chinese", 2:"American", 3:"Italian"})))
+print('tuple({1:"Chinese", 2:"American", 3:"Italian"}):\t', tuple({1:"Chinese", 2:"American", 3:"Italian"}))
 
-empty = () #空元组
-print(empty) 
+print('\n----------------other----------------')
+tup = (1, 1.2, "name", 1+2j)
+del tup #释放内存
 
-one = (1,) #一个元素的元组
-print(one)
+
+#tup[0:2] = () #tuple不可以修改
