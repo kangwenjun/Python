a89c3c7e1dc52a7c2a0e46dd473ac8a4ff0f3d86
kangwenjun <603164555@qq.com>
2020-05-04 17:10:30 +0800

    __name__ and __main__

---
+++ src/__name__.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+# coding=utf-8
+
+if __name__ == "__main__":
+	print("excuted by myself")
+else:
+	print("excuted by other py source")
---
+++ src/import.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python
+# coding=utf-8
+
+import __name__ #导入时执行“被导入模块”的程序
