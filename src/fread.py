8b7dba5cdb2f16451d801e00339a4c067fdc7ca8
kangwenjun <603164555@qq.com>
2020-05-05 04:56:03 +0800

    fread

---
+++ src/fread.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+# coding=utf-8
+
+import sys
+
+fbeg = 0
+fcur = 1
+fend = 2
+
+f = open(sys.argv[0], "r")
+f.seek(0, fend)
+file_size = f.tell()
+
+print('\n----------------read all file_size:{0}----------------'.format(file_size))
+f.seek(0, fbeg)
+print(f.read(-1))
+
+print('\n----------------read line file_size:{0}----------------'.format(file_size))
+f.seek(0, fbeg)
+someline = f.readline()
+print("\nreadline-type:", type(someline), "\n")
+while "" != someline:
+	print(someline)
+	someline = f.readline()
+	
+print('\n----------------read lines-default file_size:{0}----------------'.format(file_size))
+f.seek(0, fbeg)
+print(f.readlines())
+
+line_size = 10
+print('\n----------------read lines-{line_size} file_size:{file_size}----------------'.format(line_size=line_size, file_size=file_size))
+f.seek(0, fbeg)
+someline = f.readlines(line_size) #list
+print("\nreadlines-type:", type(someline), "\n")
+while someline != []:
+	print(someline)
+	someline = f.readlines(line_size)
+
+f.close()
