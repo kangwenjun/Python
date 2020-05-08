00b3043363f2f7ac140e0ee2fc495e0c9bee3420
kangwenjun <603164555@qq.com>
2020-05-09 07:56:59 +0800

    final

--- src/except.py
+++ src/except.py
@@ -1,11 +1,11 @@
 #! /usr/bin/env python
 # coding=utf-8
 
-import sys
-ls = [1, 2, 3, 4]
-it = iter(ls)
-while True :
-	try :
-		print(next(it), end=" ")
-	except StopIteration :
-		sys.exit()
+try:
+	1/0
+except ZeroDivisionError:
+	print("ZeroDivisionError")
+else:
+	print("other error")
+finally:
+	print("excuted anyway")
