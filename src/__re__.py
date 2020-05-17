61a561442ad0b3ad3fa865a14608b75b6f39ebf0
kangwenjun <603164555@qq.com>
2020-05-17 10:49:39 +0800

    sys

--- src/__re__.py
+++ src/__re__.py
@@ -3,6 +3,11 @@
 
 import re
 
+print('\n----------------match----------------')
+print(re.match("www", "www.kangwenjun.com").span())
+print(re.match("com", "www.kangwenjun.com"))
+
+print('\n----------------find----------------')
 print(re.findall(r"\bf[a-z]*", "which foot or hand fell fastest"))
 print(re.sub(r"(\b[a-z]+) \1", r"\1", "cat in the the hat"))
 
--- src/__sys__.py
+++ src/__sys__.py
@@ -1,9 +1,22 @@
 #! /usr/bin/env python
 # coding=utf-8
 
-import sys
+import sys;site="www.kangwenjun.com";sys.stdout.write(site);sys.stdout.write("\n")
+
+print("sys.argv:", sys.argv)
 
 sys.stdout.write("2--stdout:info\n")
 sys.stderr.write("1--stderr:warn,error\n") # 先输出stderr信息，后输出stdout信息
 sys.stdout.write("3--stdout:info\n")
+
+print('\n----------------dir----------------')
+print("dir():", dir())
+print("dir(sys):", dir(sys))
+
+'''
+1--stderr:warn,error
+2--stdout:info
+3--stdout:info
+'''
+
 sys.exit()
